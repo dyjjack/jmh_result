b9dad0b9a36f8addd6ed17974207f12e464af08d
du <105@qq.com>
2024-04-14 22:10:52 +0800

    123

--- src/components/PullRequest.vue
+++ src/components/PullRequest.vue
@@ -293,12 +293,12 @@ export default {
         dataZoom: [
           {
             type: 'inside', // 在图表内部拖动缩放
-            start: 80,
+            start: 60,
             end: 100
           },
           {
             type: 'slider', // 有一个滑动条
-            start: 80,
+            start: 60,
             end: 100
           }
         ],
